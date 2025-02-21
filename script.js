84e604ac654f63830b15f004c639656cfeb47ae1
Abhiteja1 <abhitejsunny1@gmail.com>
2025-02-21 18:24:01 +0530

    added some css and fixed some issues

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-let addCardbtn = document.querySelector(".addtask");
+let addCardbtn = document.querySelector(".addtaskbtn");
 let todoContainer = document.querySelector("#todo");
 
 addCardbtn.addEventListener("click", addtask);
 
 // adding card 
 function addtask(){
+  let parentCard = document.createElement("div");
+  parentCard.className="parentcard";
   let card = document.createElement("div");
   card.className="card";
   card.innerText="Add Task";
   card.setAttribute("contenteditable", "true");
-  todoContainer.append(card);
+  parentCard.append(card);
+  todoContainer.append(parentCard);
   card.focus(); //pointer goes to editing box automatically
 
   // empty card should delete
   card.addEventListener("blur", (eventDetails)=>{
+    
     let blurredCard = eventDetails.target;
+    let parentOfBlurredCard = blurredCard.parentElement;
     if(blurredCard.innerText.trim()==""){
-      blurredCard.remove();
+      parentOfBlurredCard.remove();
     }
   })
 
@@ -28,21 +33,21 @@ function addtask(){
     }
   })
 
-  // adding selectors to card
+  // adding selectors to card or dropdown
   let selector = document.createElement("select");
   selector.innerHTML=`
   <option value="todo">To Do</option>
   <option value="progress">Progress</option>
   <option value="completed">Completed</option>
   `
-  card.append(selector);
+  parentCard.append(selector);
 
   // choosing option then task should move according to its status
 
   selector.addEventListener("change",(eventDetails)=>{
     let selectedOption = eventDetails.target.value;
     let selectedContainer = document.querySelector(`#${selectedOption}`)
-    selectedContainer.append(card);
+    selectedContainer.append(parentCard);
   })
 }
 
